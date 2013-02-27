fc4e56f32b1328a5017fe7f714092eb441d5be7c
Ahmed--Mohsen <ahmed7890@gmail.com>
2013-02-27 11:09:08 +0200

    Add related tags to user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'set'
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -25,34 +26,47 @@ class User < ActiveRecord::Base
   has_many :created_incidents, :foreign_key => 'creator_id', :class_name => 'Incident'
   has_many :assigned_incidents , :foreign_key => 'assigned_to_id', :class_name => 'Incident'
  
-def name
-  "#{self.first_name} #{self.last_name}"
-end
-
-#check if user has manager privilage
-def isManager?
-  if self.role == "Manager"
-    return true
-  else
-    return false
+  def name
+    "#{self.first_name} #{self.last_name}"
   end
-end
 
-#calculate score given to user from incidents
-#in a given time interval
-#if default value is used for interval score field is returned
-def incidents_score(time_interval = "")
-  if time_interval == ""
-    return score
-  end 
-  calculated_score = 0
-  self.score = 0
-  self.assigned_incidents.where("created_at" => time_interval).each do |incident|
-    calculated_score += incident.score
-    self.score += incident.score
+  #check if user has manager privilage
+  def isManager?
+    if self.role == "Manager"
+      return true
+    else
+      return false
+    end
   end
-  puts "#{self.id}  #{calculated_score}"
-  return calculated_score
-end
+
+  #calculate score given to user from incidents
+  #in a given time interval
+  #if default value is used for interval score field is returned
+  def incidents_score(time_interval = "")
+    if time_interval == ""
+      return score
+    end 
+    calculated_score = 0
+    self.score = 0
+    self.assigned_incidents.where("created_at" => time_interval).each do |incident|
+      calculated_score += incident.score
+      self.score += incident.score
+    end
+    puts "#{self.id}  #{calculated_score}"
+    return calculated_score
+  end
+
+  #returns a set of the tags that have been
+  #given to that user through incidents
+  def related_tags
+    tag_names = Set.new #using set to ensure no duplicates
+    Incident.includes(:tags) #optimize DB queries
+    self.assigned_incidents.each do |incident|
+      incident.tags.each do |tag|
+        tag_names.add(tag)
+      end
+    end
+    return tag_names
+  end
   
 end
